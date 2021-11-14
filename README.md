ad82a25f58b6a269b45fd8b373b02d86ba08a3ab
Marviuz <marviuzmarvieluz@gmail.com>
2021-11-14 18:53:38 +0800

    add initial project

---
+++ README.md
@@ -0,0 +1,4 @@
+# Random question for twitch chat bots
+
+Made with https://paiza.io/
+Questions from https://opentdb.com/
---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+$api_url = [
+    'https://opentdb.com/api.php?amount=1&category=31',
+    'https://opentdb.com/api.php?amount=1&category=15'
+];
+$response = file_get_contents($api_url[array_rand($api_url)]);
+$response = json_decode($response);
+$choices = "";
+
+// var_dump($response);
+
+if ($response->results[0]->type == "boolean") {
+    $choices .= "✅ or ❌?";
+} else {
+    $all = array($response->results[0]->correct_answer, ...$response->results[0]->incorrect_answers);
+    shuffle($all);
+    
+    $choice_identifier = ["🔴","🟠","🟡","🟢"];
+    for ($i = 0; $i < count($all); $i++) {
+        $c = html_entity_decode($all[$i]);
+        $ci = $choice_identifier[$i];
+        $all[$i] = "$ci $c";
+    }
+    
+    $choices .= join(" ", $all);
+    
+}
+
+echo html_entity_decode($response->results[0]->question) . " " . $choices;
+?>
